4ffab10a8039b1de7c02e5509de82221e2c847fb
yjohnson67 <joh23004@byui.edu>
2023-04-18 16:20:45 -0500

    Numbers List Assignment #4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+             List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a series of numbers. Enter 0 to stop.");
+
+        while (true)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(number);
+        }
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        List<int> positiveNumbers = numbers.Where(n => n > 0).ToList();
+        int smallestPositive = positiveNumbers.Any() ? positiveNumbers.Min() : 0;
+
+        numbers.Sort();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        Console.WriteLine($"The sort list is: ");
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
+    
